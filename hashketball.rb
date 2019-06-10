7b9c4e2701bab5e4d1439c4a3c7ab29395513b9f
Olga Rosas <olga.rosas@gmail.com>
2019-06-10 13:43:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -257,6 +257,7 @@ def big_shoe_rebounds
                   greatest_name = player_name
                   home_away = location
                   team_level = team_data
+                  binding.pry
                   end
                 end
              end
